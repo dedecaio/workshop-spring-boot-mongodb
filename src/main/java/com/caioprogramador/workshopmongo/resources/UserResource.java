93527fc94d30167443ddd2d4b60af18d25b228ac
= <=>
2024-01-15 21:18:24 -0300

    Doing Entity User and Get Method

---
+++ src/main/java/com/caioprogramador/workshopmongo/resources/UserResource.java
@@ -0,0 +1,25 @@
+package com.caioprogramador.workshopmongo.resources;
+
+import com.caioprogramador.workshopmongo.entity.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll(){
+        User maria = new User("1", "Maria Brown", "maria@gmail.com");
+        User alex = new User("2", "Alex Green", "alex@gmail.com");
+        List<User> list = new ArrayList<>();
+        list.add(maria);
+        list.add(alex);
+        return ResponseEntity.ok(list);
+    }
+}
