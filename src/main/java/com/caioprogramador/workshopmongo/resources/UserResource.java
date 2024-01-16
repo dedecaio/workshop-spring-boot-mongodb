39efef2ee421774028d14180901e0a745116bb87
= <=>
2024-01-15 22:08:25 -0300

    Getting an user By Id

--- src/main/java/com/caioprogramador/workshopmongo/resources/UserResource.java
+++ src/main/java/com/caioprogramador/workshopmongo/resources/UserResource.java
@@ -6,6 +6,7 @@ import com.caioprogramador.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,12 @@ public class UserResource {
         List<User> list = service.findAll();
         List<UserDTO> listDto = list.stream().map(UserDTO::new).toList();
         return ResponseEntity.ok(listDto);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id){
+        User obj = service.findById(id);
+        UserDTO userDTO = new UserDTO(obj);
+        return ResponseEntity.ok(userDTO);
     }
 }
--- src/main/java/com/caioprogramador/workshopmongo/services/impl/UserServiceImpl.java
+++ src/main/java/com/caioprogramador/workshopmongo/services/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.caioprogramador.workshopmongo.services.impl;
 import com.caioprogramador.workshopmongo.entity.User;
 import com.caioprogramador.workshopmongo.repository.UserRepository;
 import com.caioprogramador.workshopmongo.services.UserService;
+import com.caioprogramador.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,5 +17,12 @@ public class UserServiceImpl implements UserService {
     @Override
     public List<User> findAll() {
         return repository.findAll();
+    }
+
+    @Override
+    public User findById(String id) {
+        User user = repository.findById(id)
+                .orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+        return user;
     }
 }
