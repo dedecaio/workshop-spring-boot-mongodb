b3cd21dbc8f2a5c2e27e2c2d67209d456e43c9b2
= <=>
2024-01-17 16:55:56 -0300

    Adding comments to posts

--- src/main/java/com/caioprogramador/workshopmongo/config/Instantiation.java
+++ src/main/java/com/caioprogramador/workshopmongo/config/Instantiation.java
@@ -1,6 +1,7 @@
 package com.caioprogramador.workshopmongo.config;
 
 import com.caioprogramador.workshopmongo.dto.AuthorDTO;
+import com.caioprogramador.workshopmongo.dto.CommentDTO;
 import com.caioprogramador.workshopmongo.entity.Post;
 import com.caioprogramador.workshopmongo.entity.User;
 import com.caioprogramador.workshopmongo.repository.PostRepository;
@@ -37,11 +38,22 @@ public class Instantiation implements CommandLineRunner {
         Post post2 = new Post(null, sdf.parse("23/03/20218"),
                 "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 
+        CommentDTO c1 = new CommentDTO("Boa viagem mano!",
+                sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite",
+                sdf.parse("22/03/2018"), new AuthorDTO(alex));
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!",
+                sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1,c2));
+        post2.getComments().add(c3);
 
         postRepository.saveAll(Arrays.asList(post1,post2));
 
         maria.getPosts().addAll(Arrays.asList(post1,post2));
         userRepository.save(maria);
+
+
 
     }
 }
